11f64029a10144bdef91a78ea7e1d0f11ce2d732
Jim Nelson <jim@jimnelson.us>
2020-05-07 02:36:17 -0500

    refactored nbt tree walker

--- examples/tree-walk.lua
+++ examples/tree-walk.lua
@@ -1,20 +1,15 @@
--- top level is an array of concatenated nbt top-tags (Often just the one compound tag)
-for k,v in ipairs(nbt) do
-    print("=== A top-of-hierarchy tag ===")
-    -- usually a compound tag
-    for k2,v2 in pairs(v) do
-        if k2=="value" and type(v2)=="table" then
-            for k3,v3 in pairs(v2) do
-            if k3=="value" and type(v3)=="table" then
-                print("--- A second-tier tag ---")
-                print("Key", k3, "Value", v3)
-            else
-                print(k3, v3)
-            end
+-- pass it a table and optinoally hierarchy number
+function printNbtTables(t, n)
+    n = n or 0
+    local s = string.rep("  ", n)
+    for k,v in pairs(t) do
+        if type(v) == "table" then
+            print(s, k, ":")
+            printNbtTables(v,n+1)
+        else
+            print(s, k, v)
         end
-    else
-        print(k2,v2)
     end
-    end
-
 end
+
+printNbtTables(nbt)

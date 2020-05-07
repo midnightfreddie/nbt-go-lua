6d923ef82215d33afff1ef9041361ab4f79888f5
Jim Nelson <jim@jimnelson.us>
2020-05-06 23:14:53 -0500

    testing working

--- nbt2lua_test.go
+++ nbt2lua_test.go
@@ -11,23 +11,22 @@ import (
 
 func TestValueConversions(t *testing.T) {
 
-	tags := []struct {
+	numberTags := []struct {
 		tagType lua.LNumber
-		lType   lua.LValueType
 		value   lua.LValue
 		nbt     []byte
 	}{
-		{1, lua.LTNumber, lua.LNumber(math.MaxInt8), []byte{1, 0, 0, 0x7f}},
-		{1, lua.LTNumber, lua.LNumber(math.MinInt8), []byte{1, 0, 0, 0x80}},
-		{2, lua.LTNumber, lua.LNumber(math.MaxInt16), []byte{2, 0, 0, 0xff, 0x7f}},
-		{2, lua.LTNumber, lua.LNumber(math.MinInt16), []byte{2, 0, 0, 0x00, 0x80}},
-		{3, lua.LTNumber, lua.LNumber(math.MaxInt32), []byte{3, 0, 0, 0xff, 0xff, 0xff, 0x7f}},
-		{3, lua.LTNumber, lua.LNumber(math.MinInt32), []byte{3, 0, 0, 0x00, 0x00, 0x00, 0x80}},
+		{1, lua.LNumber(math.MaxInt8), []byte{1, 0, 0, 0x7f}},
+		{1, lua.LNumber(math.MinInt8), []byte{1, 0, 0, 0x80}},
+		{2, lua.LNumber(math.MaxInt16), []byte{2, 0, 0, 0xff, 0x7f}},
+		{2, lua.LNumber(math.MinInt16), []byte{2, 0, 0, 0x00, 0x80}},
+		{3, lua.LNumber(math.MaxInt32), []byte{3, 0, 0, 0xff, 0xff, 0xff, 0x7f}},
+		{3, lua.LNumber(math.MinInt32), []byte{3, 0, 0, 0x00, 0x00, 0x00, 0x80}},
 	}
 
 	UseBedrockEncoding()
 	L := lua.NewState()
-	for _, tag := range tags {
+	for _, tag := range numberTags {
 		err := Nbt2Lua(tag.nbt, L)
 		if err != nil {
 			t.Error(fmt.Sprintf("Error processing nbt: `%s` NBT hex dump:\n%s", err.Error(), hex.Dump(tag.nbt)))
@@ -51,8 +50,22 @@ func TestValueConversions(t *testing.T) {
 						} else {
 							t.Error("Name is not a string:", name.Type())
 						}
-						// tagType := L.RawGet(lTag, lua.LString("tagType"))
-						// value := L.RawGet(lTag, lua.LString("value"))
+						tagType := L.RawGet(lTag, lua.LString("tagType"))
+						if tagTypeNumber, ok := tagType.(lua.LNumber); ok {
+							if tagTypeNumber != tag.tagType {
+								t.Error(fmt.Sprintf("Expected %v, got %v", tag.tagType, tagTypeNumber))
+							}
+						} else {
+							t.Error("tagType is not a number", tagType.Type())
+						}
+						value := L.RawGet(lTag, lua.LString("value"))
+						if valueTyped, ok := value.(lua.LNumber); ok {
+							if value != tag.value {
+								t.Error(fmt.Sprintf("Value expected %v, got %v", tag.value, valueTyped))
+							}
+						} else {
+							t.Error(fmt.Sprintf("Value type expected lua.LNumber, got %v", value))
+						}
 					}
 				})
 			}
